5e1c58fbafde11f252b8ef5edb75e782fef0d13a
Dieu Donne .O <dieudonne@intside.com>
2025-09-11 19:00:43 +0100

    fix: removing unnecessary migration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://easy-univ.vercel.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://easyuniv.lephenix.bj')],
     // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
